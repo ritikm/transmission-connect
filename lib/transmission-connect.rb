903f56a1922490cee84d78701e109da084d2b771
Ritik Malhotra <ritikmalhotra@gmail.com>
2013-06-10 21:56:11 -0700

    added the proper requires and added client file so that it actually works now

--- lib/transmission-connect.rb
+++ lib/transmission-connect.rb
@@ -1,2 +1,7 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
 require File.dirname(__FILE__) + '/transmission-connect/configuration'
 require File.dirname(__FILE__) + '/transmission-connect/server'
+require File.dirname(__FILE__) + '/transmission-connect/client'
